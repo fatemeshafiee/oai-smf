ecd1932c8c65548f7113a8fb6f698ee0e3095716
wangys <wangys2009bj@163.com>
2019-11-04 11:22:12 +0800

    add pdu_session_resource_release_response msg

---
+++ src/NgapSmfLayer/ng_pdu_session_resource_release_response.h
@@ -0,0 +1,10 @@
+#ifndef  NG_PDU_SESSOIN_RESOURCE_RELEASE_RESPONSE_H_
+#define  NG_PDU_SESSOIN_RESOURCE_RELEASE_RESPONSE_H_
+
+
+#include "Ngap_NGAP-PDU.h"
+Ngap_NGAP_PDU_t *make_NGAP_pdu_session_resource_release_response();
+
+
+#endif
+
