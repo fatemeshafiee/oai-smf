b30daeaf389311fed9b4cf8c18503b5b92a5db52
Tien-Thinh Nguyen <Tien-Thinh.Nguyen@eurecom.fr>
2021-09-23 11:41:47 +0200

    Fix issue for PLMN in UDM's URI

--- src/smf_app/smf_sbi.cpp
+++ src/smf_app/smf_sbi.cpp
@@ -851,7 +851,8 @@ bool smf_sbi::get_sm_data(
       std::string(inet_ntoa(*((struct in_addr*) &smf_cfg.udm_addr.ipv4_addr))) +
       ":" + std::to_string(smf_cfg.udm_addr.port) + NUDM_SDM_BASE +
       smf_cfg.udm_addr.api_version +
-      fmt::format(NUDM_SDM_GET_SM_DATA_URL, std::to_string(supi));
+      fmt::format(NUDM_SDM_GET_SM_DATA_URL, std::to_string(supi)) + query_str;
+
   Logger::smf_sbi().debug("UDM's URL: %s ", url.c_str());
 
   std::string response_data = {};
