39eaaebcc987bb3c5497502ba850c989f4812fa9
Giulio Carota <giulio.carota@eurecom.fr>
2023-10-17 15:25:15 +0200

    applying clang-format style

--- src/api-server/smf-http2-server.h
+++ src/api-server/smf-http2-server.h
@@ -64,12 +64,10 @@ class smf_http2_server {
 
   void nf_status_notify_handler(
       const NotificationData& notificationData, const response& response);
-  
+
   void create_event_subscription_handler(
       const NsmfEventExposure& smfCreateEventSubscription,
       const response& response);
-
-
 
   void get_configuration_handler(const response& response);
   void update_configuration_handler(
